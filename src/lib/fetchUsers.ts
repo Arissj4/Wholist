5e2c058f0333837cb9314c45f4478b48c1b7609e
Soheil Jamshidi <eris1376@gmail.com>
2026-05-26 16:58:27 +0200

    feat: implemented fetchUsers api, add User type

---
+++ src/lib/fetchUsers.ts
@@ -0,0 +1,35 @@
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lnt: string;
+      lng: string;
+    };
+    phone: string;
+    website: string;
+  };
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+export async function fetchUsers(): Promise<User[]> {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch users");
+  }
+
+  const data = await res.json();
+
+  return data;
+}
