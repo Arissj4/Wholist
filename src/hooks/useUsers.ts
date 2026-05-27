a4dfbf588b4e60a2f564952e0dacfe18a8eec4b7
Soheil Jamshidi <eris1376@gmail.com>
2026-05-27 17:52:42 +0200

    feat: Implemented User state hook

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,26 @@
+import { fetchUsers, User } from "@/lib/fetchUsers";
+import { useEffect, useState } from "react";
+
+type State = {
+  users: User[];
+  loading: boolean;
+  error: string | null;
+};
+
+export function useUsers() {
+  const [state, setState] = useState<State>({
+    users: [],
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    fetchUsers()
+      .then((users) => setState({ users, loading: false, error: null }))
+      .catch(() => {
+        setState({ users: [], loading: false, error: "Failed to load users" });
+      });
+  }, []);
+
+  return state;
+}
