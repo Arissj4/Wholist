0eb2e3f5840dd5ffe21babc23a9b4c1bcd4a8521
Soheil Jamshidi <eris1376@gmail.com>
2026-05-28 17:01:27 +0200

    test: fixed tests

--- src/app/__tests__/hooks/setUsers.test.ts
+++ src/app/__tests__/hooks/setUsers.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { useUsers } from "@/hooks/useUsers";
 import { fetchUsers } from "@/lib/fetchUsers";
 
