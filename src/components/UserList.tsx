49d1231234ea6a10dc0d91c09920fc068edfbdf4
Soheil Jamshidi <eris1376@gmail.com>
2026-07-06 19:51:52 +0200

    feat: implemented search input for users

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,20 +2,31 @@
 
 import { useUsers } from "@/hooks/useUsers";
 import UserCard from "./UserCard";
+import { useState } from "react";
 
 export function UserList() {
   const { users, loading, error } = useUsers();
+  const [search, setSearch] = useState("");
 
   if (loading) return <p data-testid="loading">Loading...</p>;
   if (error) return <p data-testid="error">{error}</p>;
 
   return (
-    <ul data-testid="user-list">
-      {users.map((user) => (
-        <li key={user.id}>
-          <UserCard user={user} />
-        </li>
-      ))}
-    </ul>
+    <div className="flex flex-col items-center justify-center">
+      <input
+        data-testid="search-input"
+        type="text"
+        placeholder="Search users ..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <ul className="w-full" data-testid="user-list">
+        {users.map((user) => (
+          <li key={user.id}>
+            <UserCard user={user} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
