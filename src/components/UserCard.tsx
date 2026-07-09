374ffa39b5dbf89b3e148dd8b34ba814b35ee0bc
Soheil Jamshidi <eris1376@gmail.com>
2026-07-09 18:17:03 +0200

    style: modified user card

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -7,16 +7,16 @@ type Props = {
 export default function UserCard({ user }: Props) {
   return (
     <div
-      className="user-card flex m-2 p-3 border rounded-lg"
+      className="user-card flex my-4 p-3 border rounded-lg border-gray-600 bg-[#1A1A1A] shadow-md shadow-[rgba(255,255,255,0.3)]"
       data-testid="user-card"
     >
       <div className="w-[50%]">
-        <p>Owner:</p>
+        <p className="text-amber-200 text-[18px] mb-1">Owner</p>
         <p>Name: {user.name}</p>
         <p>Email: {user.email}</p>
       </div>
       <div className="w-[50%]">
-        Company:
+        <p className="text-amber-200 text-[18px] mb-1">Company</p>
         <p>City: {user.address.city}</p>
         <p>Name: {user.company.name}</p>
       </div>
