854d869003a3411a0fbe250c5c5f0351af907038
Soheil Jamshidi <eris1376@gmail.com>
2026-07-09 18:01:24 +0200

    style: modified loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { UserList } from "@/components/UserList";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-start bg-zinc-50 font-sans dark:bg-black">
-      <main className="w-[50%]">
+    <div className="h-full flex flex-col flex-1 items-center justify-start bg-zinc-50 font-sans dark:bg-black">
+      <main className="w-[50%] h-full">
         <h1 className="text-center text-[30px] m-4">Users List</h1>
         <UserList />
       </main>
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -8,7 +8,12 @@ export function UserList() {
   const { users, loading, error } = useUsers();
   const [search, setSearch] = useState("");
 
-  if (loading) return <p data-testid="loading">Loading...</p>;
+  if (loading)
+    return (
+      <p data-testid="loading" className="h-full text-center text-[30px] mt-10">
+        Loading...
+      </p>
+    );
   if (error) return <p data-testid="error">{error}</p>;
 
   const filtered = users.filter((user) =>
@@ -18,6 +23,7 @@ export function UserList() {
   return (
     <div className="flex flex-col items-center justify-center">
       <input
+        className="bg-white text-black mb-2 p-1 rounded-sm"
         data-testid="search-input"
         type="text"
         placeholder="Search users ..."
