d13819bf4cc54d85eaa16992a624f53724ff74f4
Soheil Jamshidi <eris1376@gmail.com>
2026-08-03 15:52:16 +0200

    feat: implemented new Slice for users api

--- store/index.ts
+++ store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./searchSlice";
+import { usersAPI } from "./usersAPI";
 
 export const store = configureStore({
   reducer: {
     search: searchReducer,
+    [usersAPI.reducerPath]: usersAPI.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(usersAPI.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
---
+++ store/usersAPI.ts
@@ -0,0 +1,15 @@
+import { User } from "@/lib/fetchUsers";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+export const usersAPI = createApi({
+  reducerPath: 'usersAPI',
+  baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com'}),
+  endpoints: (builder) => ({
+    getUsers: builder.query<User[], void>({
+      query: () => '/users',
+    }),
+  }),
+});
+
+export const { useGetUsersQuery } = usersAPI;
