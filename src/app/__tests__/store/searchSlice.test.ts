b6b08878d37caec439abd35a7649cc0457495db3
Soheil Jamshidi <eris1376@gmail.com>
2026-09-01 14:24:37 +0200

    test: Implemented new test for searchSlice RS

--- src/app/__tests__/store/searchSlice.test.ts
+++ src/app/__tests__/store/searchSlice.test.ts
@@ -10,4 +10,10 @@ describe("searchSlice", () => {
     const res = reducer(initialState, { type: "unknown" });
     expect(res).toEqual(initialState);
   });
+
+  it("updates query on setQuery", () => {
+    const res = reducer(initialState, setQuery("Lemme"));
+    expect(res.query).toEqual("Lemme");
+    expect(res.statusFilter).toEqual("all");
+  });
 });
